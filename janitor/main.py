03e4b285363d14a5feba3bf1331c15607dc3c780
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2022-03-03 22:59:30 +0000

    add fake progress bar code

--- janitor/main.py
+++ janitor/main.py
@@ -1,19 +1,35 @@
+import time
+
 import typer
 
 app = typer.Typer()
 
 
 @app.command()
-def plan(verbose: bool = False):
+def scan(verbose: bool = False):
     if verbose:
-        typer.echo("I am the janitor")
-    typer.echo("This is my plan... Nothing yet.")
+        typer.echo("I am the janitor and I am about to look around for stuff to do")
+    typer.echo("I am done scanning for stuff to do")
 
 
 @app.command()
 def apply():
-    plan()
-    typer.echo("I am now doing things according to plan!")
+    scan()
+    typer.echo(
+        "I am now doing things according to the information I got while scanning!"
+    )
+
+
+@app.command()
+def toolbox():
+    typer.echo("I am about to progress a lot of stuff!")
+    total = 0
+    with typer.progressbar(range(1000)) as progress:
+        for value in progress:
+            # Fake processing time
+            time.sleep(0.01)
+            total += 1
+    typer.echo(f"Processed {total} things.")
 
 
 def main():
