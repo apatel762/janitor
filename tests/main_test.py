018eca3f4cb4e438dac8a83dae12805f6a97feae
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2022-03-04 18:40:11 +0000

    start from blank slate on 'scan' sub-command

--- tests/main_test.py
+++ tests/main_test.py
@@ -8,14 +8,4 @@ runner = CliRunner()
 def test_scan():
     result = runner.invoke(app, ["scan"])
     assert result.exit_code == 0
-    assert "I am done scanning for stuff to do" in result.stdout
-
-
-def test_scan_verbose_has_extra_output():
-    result = runner.invoke(app, ["scan", "--verbose"])
-    assert result.exit_code == 0
-    assert (
-        "I am the janitor and I am about to look around for stuff to do"
-        in result.stdout
-    )
-    assert "I am done scanning for stuff to do" in result.stdout
+    assert "Done scanning" in result.stdout
