d38441efde949adfee5dd286e3100bf100730bb6
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2022-03-04 18:37:51 +0000

    add basic tests using typer.testing.CliRunner

---
+++ tests/janitor_test.py
@@ -0,0 +1,5 @@
+import janitor
+
+
+def test_version():
+    assert janitor.__version__ == "0.1.0"
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,5 +1,21 @@
-from janitor import __version__
+from typer.testing import CliRunner
+
+from janitor.main import app
+
+runner = CliRunner()
 
 
-def test_version():
-    assert __version__ == "0.1.0"
+def test_scan():
+    result = runner.invoke(app, ["scan"])
+    assert result.exit_code == 0
+    assert "I am done scanning for stuff to do" in result.stdout
+
+
+def test_scan_verbose_has_extra_output():
+    result = runner.invoke(app, ["scan", "--verbose"])
+    assert result.exit_code == 0
+    assert (
+        "I am the janitor and I am about to look around for stuff to do"
+        in result.stdout
+    )
+    assert "I am done scanning for stuff to do" in result.stdout
