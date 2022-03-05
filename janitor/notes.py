ff0e5e8755514472e51e2b0716fdd2a0eaf8db41
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2022-03-05 13:50:10 +0000

    flesh out the crawler code

--- janitor/notes.py
+++ janitor/notes.py
@@ -1,9 +1,33 @@
-from pathlib import Path
+from os import DirEntry
+from typing import List
 
 
 class Note:
-    def __init__(self, file_path: Path):
-        self.file_path = file_path
+    def __init__(self, path: DirEntry):
+        """
+        :param path: A path to the Note file where this Note is stored.
+        """
+        self.path = path
+        self.backlinks: List[Note] = []
+        self.forward_links: List[ForwardLink] = []
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}[{self.path.name}]"
+
+
+class ForwardLink:
+    """
+    A forward link is a link that originates from a Note and takes you to
+    another Note.
+    """
+
+    def __init__(self, origin: Note, destination: Note):
+        """
+        :param origin: The note that contains the link.
+        :param destination: The note that the link is pointing to.
+        """
+        self.origin: Note = origin
+        self.destination: Note = destination
 
     def __repr__(self):
-        return f"{self.__class__.__name__}[{self.file_path.name}]"
+        return f"{self.__class__.__name__}[{self.origin}, {self.destination}]"
--- tests/indexer_test.py
+++ tests/indexer_test.py
@@ -1,7 +1,14 @@
-from janitor.indexer import Indexer
+from janitor.indexer import Index
 
 
-def test_indexer__when_created__repr_string_contains_class_name():
-    index = Indexer()
+def test_indexer__when_created__repr_string_indicates_that_its_empty():
+    index = Index()
 
-    assert "Indexer[" and "]" in repr(index)
+    assert repr(index) == "Index.empty"
+
+
+def test_indexer__when_created_with_elements__repr_string_shows_size():
+    index = Index()
+    index.notes = [1, 2]
+
+    assert repr(index) == "Index(size=2)"
