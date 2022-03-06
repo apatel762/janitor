6ab710d39cd4595c87d9eb07b71117bff288f865
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2022-03-06 10:01:21 +0000

    add error handling to indexer.py

--- janitor/indexer.py
+++ janitor/indexer.py
@@ -27,21 +27,31 @@ class Index:
     def __init__(self) -> None:
         self.__notes: List[Note] = []
 
-    def register(self, note: Note) -> None:
+    def register(self, note: Optional[Note]) -> None:
         """
         Registers a Note with the index (a fancy way of saying that it adds
         it to the Notes list).
 
         :param note: A Note to add to the index
         """
+        if note is None:
+            raise NoteIndexerError(
+                f"Cannot register a {type(note)}-type object with the Index."
+            )
+
         self.__notes.append(note)
 
-    def dump(self, location: PathLike) -> bool:
+    def dump(self, location: Optional[PathLike]) -> bool:
         """
         Dump the indexed content to disk in the JSON format.
 
         :return: True if the operation was successful, otherwise False.
         """
+        if location is None:
+            raise NoteIndexerError(
+                "Cannot dump the Index to disk without providing a location first."
+            )
+
         target_location: PathLike = Path(os.path.join(location, "index.json"))
         typer.echo(f"Dumping {self} to: {target_location}")
 
@@ -55,13 +65,18 @@ class Index:
 
         return True
 
-    def search_for_note(self, file_name: str) -> Optional[Note]:
+    def search_for_note(self, file_name: Optional[str]) -> Optional[Note]:
         """
         Search the Index for a Note using a file name.
 
         :param file_name: The filename of the Note that you are looking for
         :return: The Note if we have indexed it, otherwise None
         """
+        if file_name is None or len(file_name) == 0:
+            raise NoteIndexerError(
+                f"Cannot search for Note in Index with name: {file_name}"
+            )
+
         for note in self:
             if note.path.name == file_name:
                 return note
